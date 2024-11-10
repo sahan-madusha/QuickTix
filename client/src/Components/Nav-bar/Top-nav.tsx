44f459b8868fe19e68f57838be0ebc3de3eb8382
sahan madusha <sahanmadusha001@gmail.com>
2024-11-10 22:00:44 +0530

    project env setup and home page design

---
+++ client/src/Components/Nav-bar/Top-nav.tsx
@@ -0,0 +1,98 @@
+import { scrollToTop } from "../../Lib/Utils";
+import {
+  EMAIL,
+  FACEBOOK,
+  HOMEPAGEURL,
+  IMAGE_URL,
+  INSTERGRAM,
+  WHATSAPP,
+} from "../../../src/Constant";
+import {
+  FacebookFilled,
+  InstagramFilled,
+  WhatsAppOutlined,
+} from "@ant-design/icons";
+import { Avatar, Tooltip } from "antd";
+import { AtSign, LogIn } from "lucide-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const TopNav = () => {
+  const SocialMeadia = () => {
+    return (
+      <>
+        <Tooltip placement="bottom" title={"Visit Our Facebook Prifile"}>
+          <Link to={FACEBOOK} target="_blank" className="mx-2">
+            <FacebookFilled className="text-blue-700 text-2xl	" />
+          </Link>
+        </Tooltip>
+
+        <Tooltip placement="bottom" title={"Follow Us On Instagram"}>
+          <Link to={INSTERGRAM} target="_blank" className="mx-2">
+            <InstagramFilled className="text-2xl" />
+          </Link>
+        </Tooltip>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div className="w-full bg-gray-200 py-4">
+        <div className="row flex flex-col md:flex-row justify-around col-span-2 items-center">
+          <div className="col-span-1 mt-1">
+            <ul className="flex justify-between items-center pb-0 mb-0">
+              <li className="mx-3 flex font-bold">
+                <Tooltip placement="bottom" title={"Home Page"}>
+                  <Link to={HOMEPAGEURL}>
+                    <Avatar
+                      src={`${IMAGE_URL}/logo.jpg`}
+                      size={35}
+                      className="hover:cursor-pointer"
+                    />
+                  </Link>
+                </Tooltip>
+              </li>
+              <li className="mx-3 flex font-bold">
+                <Tooltip placement="bottom" title={"Contact us via whatsapp"}>
+                  <WhatsAppOutlined className="text-green-500 w-4" />
+                  <span className="ms-1 text-sm">{WHATSAPP}</span>
+                </Tooltip>
+              </li>
+              <li className="mx-3 ">
+                <Tooltip placement="bottom" title={"Contact us via Email"}>
+                  <div className="flex items-center font-bold">
+                    <AtSign className="text-red-500 w-4" />
+                    <a className="ms-1 text-sm" href={`mailto:${EMAIL}`}>
+                      {EMAIL}
+                    </a>
+                  </div>
+                </Tooltip>
+              </li>
+            </ul>
+          </div>
+
+          <div className="hidden md:flex justify-between ">
+            <SocialMeadia />
+          </div>
+
+          <div className="flex justify-around items-center mt-3 pb-0 md:mt-0">
+            <div className="flex md:hidden justify-between ">
+              <SocialMeadia />
+            </div>
+
+            <div>
+              <Link
+                to={""}
+                onClick={scrollToTop}
+                className="flex items-center font-bold"
+              >
+                Sign In <LogIn className="mx-1" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ client/src/Constant/config.ts
@@ -0,0 +1,55 @@
+const env = process.env.REACT_APP_APP_ENV;
+const commonConfig = {
+  webName: "quicktix.com",
+
+  facebook: "https://web.facebook.com/profile.php?id=61560034121554",
+  instergram: "https://www.instagram.com/beachsrilankatravel/",
+
+  address: "No.248/ B, Hithgoda ,Wettewa, Galagedara , Sri lanka.",
+  mobile: "+94771617400",
+  email: "quicktix@gmail.com",
+  whatsapp: "+94771617400",
+  devsSite: "https://taprodev.com/",
+
+  noImage: "No_Image_Available.jpg",
+};
+
+const baseDomainProd = window.location.hostname.includes("www")
+  ? "https://www.trainedguide.com"
+  : "https://trainedguide.com";
+
+const baseDomainQa = window.location.hostname.includes("www")
+  ? "https://www.qa.trainedguide.com"
+  : "https://qa.trainedguide.com";
+
+const environments = {
+  development: {
+    webLink: "http://localhost:3000",
+    server: "http://localhost/app/newtrainedguide/trained-guide/server",
+    serverapi: "http://localhost/app/newtrainedguide/trained-guide/server/Api",
+    imagepath: `http://localhost/app/newtrainedguide/trained-guide/image`,
+  },
+  qa: {
+    webLink: `${baseDomainQa}`,
+    server: `${baseDomainQa}/server`,
+    serverapi: `${baseDomainQa}/server/Api`,
+    imagepath: `${baseDomainQa}/image`,
+  },
+  production: {
+    webLink: `${baseDomainProd}`,
+    server: `${baseDomainProd}/server`,
+    serverapi: `${baseDomainProd}/server/Api`,
+    imagepath: `${baseDomainProd}/image`,
+  },
+};
+
+// Merge common configuration with environment-specific configuration
+const config = {
+  ...commonConfig,
+  ...environments[env],
+
+  // Web routes
+  homepageurl: "/",
+};
+
+export default config;
