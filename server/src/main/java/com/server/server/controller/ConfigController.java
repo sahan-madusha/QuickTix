b4f55eb42b2385aa37f1a5f5f4df2e79f6ceffb9
sahan madusha <sahanmadusha001@gmail.com>
2024-12-02 23:50:23 +0530

    changes in socket

--- server/src/main/java/com/server/server/controller/ConfigController.java
+++ server/src/main/java/com/server/server/controller/ConfigController.java
@@ -7,7 +7,6 @@ import com.server.server.util.MessageResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
