75a09f2d864c25c613089e69aa834793ea121a6f
sahan madusha <sahanmadusha001@gmail.com>
2024-12-06 15:19:35 +0530

    implement a thread for ticket adding feature

---
+++ server/src/main/java/com/server/server/service/TicketRunnable.java
@@ -0,0 +1,73 @@
+package com.server.server.service;
+
+import com.server.server.dto.TicketsDto;
+import com.server.server.entity.Events;
+import com.server.server.entity.Tickets;
+import com.server.server.entity.TicketsLog;
+import com.server.server.entity.User;
+import com.server.server.repository.EventRepository;
+import com.server.server.repository.TicketsLogRepository;
+import com.server.server.repository.TicketsRepository;
+import com.server.server.repository.UserRepository;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Component
+@Data
+@NoArgsConstructor(force = true)
+public class TicketRunnable implements Runnable {
+
+    private final TicketsDto ticketsDto;
+
+    private final TicketsRepository ticketsRepository;
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
+    private final TicketsLogRepository ticketsLogRepository;
+
+    public TicketRunnable(TicketsDto ticketsDto, TicketsRepository ticketsRepository,
+                          UserRepository userRepository, EventRepository eventRepository,
+                          TicketsLogRepository ticketsLogRepository) {
+        this.ticketsDto = ticketsDto;
+        this.ticketsRepository = ticketsRepository;
+        this.userRepository = userRepository;
+        this.eventRepository = eventRepository;
+        this.ticketsLogRepository = ticketsLogRepository;
+    }
+
+    @Override
+    public void run() {
+        User eventUser = userRepository.findById(ticketsDto.getUserId()).orElse(null);
+        Events eventData = eventRepository.findById(ticketsDto.getEventId()).orElse(null);
+
+        if (eventUser != null && eventData != null) {
+            Tickets ticketEntity = new Tickets();
+            ticketEntity.setName(ticketsDto.getName());
+            ticketEntity.setPrice(ticketsDto.getPrice());
+            ticketEntity.setDescription(ticketsDto.getDescription());
+            ticketEntity.setEvent(eventData);
+            ticketEntity.setUser(eventUser);
+            ticketEntity.setQty(ticketsDto.getQty());
+            ticketsRepository.save(ticketEntity);
+
+            addTicketLog(ticketEntity, eventUser, eventData, ticketsDto.getQty());
+        }
+    }
+
+    @Async
+    public void addTicketLog(Tickets ticketEntity, User user, Events event, int qty) {
+        TicketsLog ticketsLog = new TicketsLog();
+        ticketsLog.setCount(qty);
+        ticketsLog.setDate(LocalDate.now());
+        ticketsLog.setTime(LocalTime.now());
+        ticketsLog.setUserRole(user.getRole());
+        ticketsLog.setUser(user);
+        ticketsLog.setEvent(event);
+        ticketsLog.setTicket(ticketEntity);
+        ticketsLogRepository.save(ticketsLog);
+    }
+}
--- server/src/main/java/com/server/server/service/TicketService.java
+++ server/src/main/java/com/server/server/service/TicketService.java
@@ -1,62 +1,31 @@
 package com.server.server.service;
 
 import com.server.server.dto.TicketsDto;
-import com.server.server.entity.Events;
-import com.server.server.entity.Tickets;
-import com.server.server.entity.TicketsLog;
-import com.server.server.entity.User;
 import com.server.server.repository.EventRepository;
 import com.server.server.repository.TicketsLogRepository;
 import com.server.server.repository.TicketsRepository;
 import com.server.server.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @Service
 public class TicketService {
-    @Autowired
-    private TicketsRepository ticketsRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+    private final TicketsRepository ticketsRepository;
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
+    private final TicketsLogRepository ticketsLogRepository;
 
-    @Autowired
-    private EventRepository eventRepository;
-
-    @Autowired
-    private TicketsLogRepository ticketsLogRepository;
-
-
-    public void addTicket(TicketsDto ticketDto) {
-        User eventUser = userRepository.findById(ticketDto.getUserId()).get();
-        Events eventData = eventRepository.findById(ticketDto.getEventId()).get();
-
-        Tickets ticketEntity = new Tickets();
-        ticketEntity.setName(ticketDto.getName());
-        ticketEntity.setPrice(ticketDto.getPrice());
-        ticketEntity.setDescription(ticketDto.getDescription());
-        ticketEntity.setEvent(eventData);
-        ticketEntity.setUser(eventUser);
-        ticketEntity.setQty(ticketDto.getQty());
-        ticketsRepository.save(ticketEntity);
-
-        addTicketLogAsync(ticketEntity, eventUser, eventData, ticketDto.getQty());
+    public TicketService(TicketsRepository ticketsRepository, UserRepository userRepository,
+                         EventRepository eventRepository, TicketsLogRepository ticketsLogRepository) {
+        this.ticketsRepository = ticketsRepository;
+        this.userRepository = userRepository;
+        this.eventRepository = eventRepository;
+        this.ticketsLogRepository = ticketsLogRepository;
     }
 
-    @Async
-    public void addTicketLogAsync(Tickets ticketEntity, User user, Events event, int qty) {
-        TicketsLog ticketsLog = new TicketsLog();
-        ticketsLog.setCount(qty);
-        ticketsLog.setDate(LocalDate.now());
-        ticketsLog.setTime(LocalTime.now());
-        ticketsLog.setUserRole(user.getRole());
-        ticketsLog.setUser(user);
-        ticketsLog.setEvent(event);
-        ticketsLog.setTicket(ticketEntity);
-        ticketsLogRepository.save(ticketsLog);
+    public void addTicket(TicketsDto ticketDto) {
+        TicketRunnable ticketRunnable = new TicketRunnable(ticketDto, ticketsRepository, userRepository, eventRepository, ticketsLogRepository);
+        Thread thread = new Thread(ticketRunnable);
+        thread.start();
     }
 }
